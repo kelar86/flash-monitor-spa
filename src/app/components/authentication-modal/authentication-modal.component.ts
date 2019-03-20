54f3be4a12eb424141c6e58e94dd1f6df9c04e87
volynshchikov <volynshchikov@aigeo.ru>
2019-03-20 12:43:00 +0700

    login to modal window

--- src/app/components/authentication-modal/authentication-modal.component.ts
+++ src/app/components/authentication-modal/authentication-modal.component.ts
@@ -1,41 +1,93 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { first } from 'rxjs/operators';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { AuthenticationService } from 'src/app/services/authentication.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-authentication-modal',
   template: `
-    <div class="modal-header">
-      <h4 class="modal-title">Вход в систему</h4>
-      <button type="button" class="close" aria-label="Close" (click)="activeModal.dismiss('Cross click')">
-        <span aria-hidden="true">&times;</span>
-      </button>
-    </div>
+
     <div class="modal-body">
 
-    <form>
-      <div class="form-group">
-        <label for="login">Имя пользователя:</label>
-          <input type="text" class="form-control" id="login"  aria-describedby="userLogin" placeholder="Имя пользователя">  
-      </div>
-      
-      <div class="form-group">
-        <label for="password" >Пароль:</label>
-          <input type="password" class="form-control"  id="password" placeholder="Пароль">
-      </div>
-     
-      <button type="submit" class="btn btn-primary">Войти</button>
-    </form>
-      
+    <form [formGroup]="loginForm" (ngSubmit)="onSubmit()">
+    <div class="form-group">
+        <label for="username">Имя пользователя:</label>
+        <input type="text" formControlName="username" class="form-control" [ngClass]="{ 'is-invalid': submitted && f.username.errors }" />
+        <div *ngIf="submitted && f.username.errors" class="invalid-feedback">
+            <div *ngIf="f.username.errors.required">Обязательное поле</div>
+        </div>
+    </div>
+    <div class="form-group">
+        <label for="password">Пароль:</label>
+        <input type="password" formControlName="password" class="form-control" [ngClass]="{ 'is-invalid': submitted && f.password.errors }" />
+        <div *ngIf="submitted && f.password.errors" class="invalid-feedback">
+            <div *ngIf="f.password.errors.required">Обязательное поле</div>
+        </div>
+    </div>
+    <div class="form-group">
+        <button [disabled]="loading" class="btn btn-primary">Войти</button>
+    </div>
+    <div *ngIf="error" class="alert alert-danger">{{error}}</div>
+  </form>
+
   </div>
-   
+
   `,
   styles: []
 })
 export class AuthenticationModalComponent implements OnInit {
 
-  constructor(public activeModal: NgbActiveModal) { }
+  loginForm: FormGroup;
+  loading = false;
+  submitted = false;
+  returnUrl: string;
+  error = '';
+
+  constructor(
+      private formBuilder: FormBuilder,
+      private route: ActivatedRoute,
+      private router: Router,
+      private authenticationService: AuthenticationService,
+      private activeModal: NgbActiveModal) {}
 
   ngOnInit() {
+      this.loginForm = this.formBuilder.group({
+          username: ['', Validators.required],
+          password: ['', Validators.required]
+      });
+
+      // reset login status
+      this.authenticationService.logout();
+
+      // get return url from route parameters or default to '/'
+      this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+  }
+
+  // convenience getter for easy access to form fields
+  get f() { return this.loginForm.controls; }
+
+  onSubmit() {
+      this.submitted = true;
+
+      // stop here if form is invalid
+      if (this.loginForm.invalid) {
+          return;
+      }
+
+      this.loading = true;
+      this.authenticationService.login(this.f.username.value, this.f.password.value)
+          .pipe(first())
+          .subscribe(
+              data => {
+                  this.router.navigate([this.returnUrl]);
+                  this.activeModal.close('');
+              },
+              error => {
+                  this.error = error;
+                  this.loading = false;
+              });
   }
 
 }
--- src/app/components/login-page/login-page.component.ts
+++ src/app/components/login-page/login-page.component.ts
@@ -9,11 +9,9 @@ import { first } from 'rxjs/operators';
 @Component({
   selector: 'app-login-page',
   template: `
-  <div class="alert alert-info">
-  Username: test<br />
-  Password: test
-  </div>
-  <h2>Login</h2>
+
+
+  <!--
   <form [formGroup]="loginForm" (ngSubmit)="onSubmit()">
     <div class="form-group">
         <label for="username">Имя пользователя:</label>
@@ -34,56 +32,16 @@ import { first } from 'rxjs/operators';
     </div>
     <div *ngIf="error" class="alert alert-danger">{{error}}</div>
   </form>
+  -->
   `,
   styles: []
 })
 export class LoginPageComponent implements OnInit {
-  loginForm: FormGroup;
-    loading = false;
-    submitted = false;
-    returnUrl: string;
-    error = '';
 
-    constructor(
-        private formBuilder: FormBuilder,
-        private route: ActivatedRoute,
-        private router: Router,
-        private authenticationService: AuthenticationService) {}
+    constructor(private modalService: NgbModal) {}
 
     ngOnInit() {
-        this.loginForm = this.formBuilder.group({
-            username: ['', Validators.required],
-            password: ['', Validators.required]
-        });
+      this.modalService.open(AuthenticationModalComponent, {backdrop: 'static', keyboard: false});
 
-        // reset login status
-        this.authenticationService.logout();
-
-        // get return url from route parameters or default to '/'
-        this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
-    }
-
-    // convenience getter for easy access to form fields
-    get f() { return this.loginForm.controls; }
-
-    onSubmit() {
-        this.submitted = true;
-
-        // stop here if form is invalid
-        if (this.loginForm.invalid) {
-            return;
-        }
-
-        this.loading = true;
-        this.authenticationService.login(this.f.username.value, this.f.password.value)
-            .pipe(first())
-            .subscribe(
-                data => {
-                    this.router.navigate([this.returnUrl]);
-                },
-                error => {
-                    this.error = error;
-                    this.loading = false;
-                });
     }
 }
